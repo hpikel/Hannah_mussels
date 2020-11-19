2321d05085dda54be2afd172cb97eedeaac5a90a
hpikel <hannahpikel16@gmail.com>
2020-11-19 14:00:14 -0800

    Setting up the files and .Rmd of the project and reading in some data

---
+++ scripts/reading_data.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+
+mussel_size_data <- read.csv("data/Copy of MusselSizeAll.csv")
+mussel_size_data
+
+mussel_bed_data <- read.csv("data/Copy of mussel_bed_limits.csv")
+mussel_bed_data
+
+
+species_comp_data <- read.csv("data/Copy of Spp_composition_all_years_stacked.csv")
+species_comp_data
